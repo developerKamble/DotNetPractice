04998e07382c4bf987c11514b6ea1e11ea30e2d5
developerKamble <komaltanuja2714@gmail.com>
2022-09-23 20:33:08 +0530

    first cSharp project

---
+++ Project1/Project1/Program.cs
@@ -0,0 +1,18 @@
+﻿class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        Console.Write("HIIIII");
+        //programs 
+        Console.WriteLine("Type casting ");
+        int a = 10;
+        double d = a; // implicit casting
+        Console.WriteLine(d);
+
+        double dd = 9.89;
+        int aa = (int)dd; // explicit casting 
+        Console.WriteLine(dd);
+        Console.WriteLine(aa);
+    }
+}
